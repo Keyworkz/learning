c2de1893977b608dde69352a250976ecf054021c
Keyworkz <info@keyworkz.nl>
2024-08-04 13:39:45 +0200

    Applied Country Card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import "./App.css";
-import textView from "./assets/views/textView/textView";
+import "./App.scss";
+import "./assets/styles/general.scss";
+import countryCard from "./assets/views/countryCard/countryCard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <textView />
+        {countryCard({
+          imgSrc: "https://picsum.photos/200/300",
+          name: "Country",
+          population: 23,
+          buttonText: "Click me",
+          callback: null,
+        })}
       </header>
     </div>
   );
--- src/assets/views/textView/textView.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function textView() {
-  return <h1>Hello Rian!</h1>;
-}
-
-export default textView;
