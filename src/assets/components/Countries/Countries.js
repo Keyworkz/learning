958c0f41e6ddd5911032ede1befbc203f6e8a3ad
Keyworkz <info@keyworkz.nl>
2024-08-05 23:12:50 +0200

    applied load all button

--- src/assets/components/Countries/Countries.js
+++ src/assets/components/Countries/Countries.js
@@ -1,45 +1,51 @@
 import React, { Component } from "react";
 import "./Countries.scss";
-import CONFIG from "../../Config";
+import { _initCountries, loadMoreCountries } from "./CountriesUtils.js";
 
 import countryCard from "../../views/countryCard/countryCard";
 
 class Countries extends Component {
+  loadMoreCountries = () => loadMoreCountries(this);
+
   constructor() {
     super();
+
     this.state = {
       allCountries: [],
+      visibleCountries: [],
+      countriesVisibleOnLoad: 6,
       countryListError: false,
     };
 
-    this._initCountries(this);
-  }
-
-  _initCountries(context) {
-    fetch(CONFIG.api)
-      .then((resp) => resp.json())
-      .then(function (data) {
-        context.setState((state) => ({ allCountries: data }));
-      })
-      .catch(function (error) {
-        console.error(error);
-        context.setState((state) => ({ countryListError: true }));
-      });
+    _initCountries(this);
   }
 
   render() {
     return (
-      <section className="countries">
-        {this.state.allCountries.map((country, index) => {
-          return countryCard({
-            imgSrc: country.flags.svg,
-            name: country.name.common,
-            population: country.population,
-            buttonText: "View Details",
-            callback: null,
-          });
+      <div className="countries__country-list">
+        {this.state.visibleCountries.map((country, index) => {
+          return (
+            <React.Fragment key={index}>
+              {countryCard({
+                imgSrc: country.flags.svg,
+                name: country.name.common,
+                population: country.population,
+                buttonText: "View Details",
+                callback: null,
+              })}
+            </React.Fragment>
+          );
         })}
-      </section>
+        {this.state.allCountries.length >
+          this.state.visibleCountries.length && (
+          <button
+            className="button button--primary button--load"
+            onClick={this.loadMoreCountries}
+          >
+            Load all
+          </button>
+        )}
+      </div>
     );
   }
 }
